c5b89c619434e337232c55829e98b46435e5f18e
root <schmmidt89u@informatik.uni-marburg.de>
2016-05-12 13:37:58 +0000

    parallel checking for current time on qemu node

--- qemu_timing.py
+++ qemu_timing.py
@@ -3,18 +3,26 @@
 from subprocess import check_output
 import os
 from pprint import pprint
+import re
+from shlex import split
+from collections import OrderedDict
 
-os.system("./miniworld-execute-all /usr/bin/date +%s%3N | tail -n +2 | grep -v '# HOST' > qemu_dates.txt")
-
-#times = check_output('./miniworld-execute-all /usr/bin/date +%s%3N | ')
+os.system('./miniworld-execute-par "/usr/bin/date +%s%3N" 2> qemu_dates.txt')
 
 with open("qemu_dates.txt", "r") as f:
 	times = f.read()
-	times = times.split("\n")[:-1]
-	print "times from qemu nodes: \n%s" % times
+	#print "times from qemu nodes: \n%s" % times
+        times = re.findall("(\d+)\s+[:]\s+(\d+)", times)
+        #print times
+        times = sorted(map(lambda (x,y) : (int(x), int(y)), times))
+        #print times
+
+times_by_lines = OrderedDict(times).values()
+#print times_by_lines
 
 def get_pairwise_time_diff():
 	print "time diff:"
-	pprint(map(lambda (cnt, (x, y)): ("%s,%s" % (cnt, cnt+1), int(y)-int(x)), enumerate(zip(times, times[1:]), 1)))
+	diff_list = map(lambda (cnt, (x, y)): ("%s,%s" % (cnt, cnt+1), y-x), enumerate(zip(times_by_lines, times_by_lines[1:]), 1))
+        pprint(diff_list)
 
 get_pairwise_time_diff()
